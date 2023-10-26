7e9d1c41fd1285a3870cec135390b71035f204ca
Maryam-Zubair <123619589+Maryam-Zubair@users.noreply.github.com>
2023-10-25 23:01:06 -0700

    Add files via upload

---
+++ Project/midterm/trackerapi.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+export const trackid = async (trackingid) =>{
+    const apiKey = process.env.DHL_API_Key
+    const apiurl = `https://api-eu.dhl.com/track/shipments?trackingNumber=${trackingid}`
+    try {
+        const trackResponse = await axios.get(apiurl ,{
+    headers :{
+        'Accept': 'application/json',
+        'DHL-API-Key': apiKey
+    }}
+    )
+    return {"tracking detaild" : trackResponse?.data}
+}
+    catch (error) {
+        console.log('Error:', error);
+    }
+}
